4301efb224bb629b845148644ba96590afbe81ec
Bassel-alkhalaf <123759979+Bassel-alkhalaf@users.noreply.github.com>
2024-09-22 22:00:24 -0400

    returned the comments attributes

--- backend/Models/Comment.cs
+++ backend/Models/Comment.cs
@@ -5,13 +5,18 @@ namespace backend.Models
     public class Comment
     {
         [FirestoreDocumentId]
+        public string CommentId { get; set; }
         
         [FirestoreProperty]
+        public string PostId { get; set; }
         
         [FirestoreProperty]
+        public string AuthorId { get; set; }
         
         [FirestoreProperty]
+        public string Text { get; set; }
         
         [FirestoreProperty]
+        public DateTime CreatedAt { get; set; }
     }
 }
