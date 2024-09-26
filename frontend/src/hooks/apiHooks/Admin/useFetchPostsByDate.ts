7b268dc19116197a302a96b6708c1504f7487ab8
james <jamesfallouh@outlook.com>
2024-09-26 09:45:56 -0400

    messing update

--- frontend/src/hooks/apiHooks/Admin/useFetchPostsByDate.ts
+++ frontend/src/hooks/apiHooks/Admin/useFetchPostsByDate.ts
@@ -5,10 +5,7 @@ import { Post } from '../../../types/post.type';
 // Correct usage of `useQuery`
 export const useFetchPostsByDate = (selectedDate: string) => {
   return useQuery<Post[], Error>({
-    queryKey: ['posts', selectedDate],
-    queryFn: () => fetchPostsByDate(selectedDate),
-    onError: (error) => {
-      console.error('Error in useFetchPostsByDate:', error); // Log any error to the console
-    },
+    queryKey: ['posts', selectedDate], // Correct use of queryKey
+    queryFn: () => fetchPostsByDate(selectedDate), // Correct query function
   });
 };
