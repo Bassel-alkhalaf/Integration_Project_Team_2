9aa841060e57484fbf304ac9e10804b413e2a086
Marc-J-L <marcjieli@gmail.com>
2024-09-28 15:19:21 -0400

    fixed

--- frontend/src/components/ApiMusic.tsx
+++ frontend/src/components/ApiMusic.tsx
@@ -63,7 +63,7 @@ export function PlaylistComponent() {
           width="100%"
           height="100%"
           style={{ minHeight: '360px' }}
-          frameBorder="0"
+          
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
           loading="lazy"
         />
