2c13ac70a35563ef4307fbc6fa26eeccf9906fd5
Xintang ZHANG <xtzhang1994@gmail.com>
2024-10-02 20:34:42 -0400

    bug fix: access-denied page always show up on login

--- frontend/src/routes/RoleBasedRoute.tsx
+++ frontend/src/routes/RoleBasedRoute.tsx
@@ -9,7 +9,6 @@
 // export function RoleBasedRoute({ allowedRoles }: PropsI) {
 // 	const { user } = useAuth();
 
-	
 // 	if (user && allowedRoles.includes(Role.Guest) && allowedRoles.length === 1) {
 // 		return <Navigate to='/' replace />;
 // 	}
@@ -25,33 +24,37 @@
 // 	return <Outlet />;
 // }
 
-
 import { Navigate, Outlet } from 'react-router-dom';
+import { Loading } from '../components';
 import { useAuth } from '../contexts';
 import { Role } from '../types';
 
 interface PropsI {
-  allowedRoles: Role[];
+	allowedRoles: Role[];
 }
 
 export function RoleBasedRoute({ allowedRoles }: PropsI) {
-  const { user } = useAuth();
+	const { user, isAuthLoading, setIsAuthLoading } = useAuth();
 
-  // Redirect to 'account-suspended' if the user's role is 'Suspended'
-  if (user && user.role === Role.Suspended) {
-    return <Navigate to="/account-suspended" replace />;
-  }
+	if (isAuthLoading && !allowedRoles.includes(Role.Guest)) return <Loading />;
 
-  // Check if the user's role is allowed for the current route
-  if (user && !allowedRoles.includes(user.role as Role)) {
-    return <Navigate to="/access-denied" replace />;
-  }
+	if (isAuthLoading && allowedRoles.includes(Role.Guest)) setIsAuthLoading(false);
 
-  // If the user is not authenticated and the route is not accessible to guests, redirect to login
-  if (!user && !allowedRoles.includes(Role.Guest)) {
-    return <Navigate to="/login" replace />;
-  }
+	// Redirect to 'account-suspended' if the user's role is 'Suspended'
+	if (user && user.role === Role.Suspended) {
+		return <Navigate to='/account-suspended' replace />;
+	}
 
-  // Render the child routes if the user is allowed
-  return <Outlet />;
+	// Check if the user's role is allowed for the current route
+	if (user && !allowedRoles.includes(user.role as Role)) {
+		return <Navigate to='/access-denied' replace />;
+	}
+
+	// If the user is not authenticated and the route is not accessible to guests, redirect to login
+	if (!user && !allowedRoles.includes(Role.Guest)) {
+		return <Navigate to='/login' replace />;
+	}
+
+	// Render the child routes if the user is allowed
+	return <Outlet />;
 }
